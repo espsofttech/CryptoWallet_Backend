c34e3b0d738b50da792ea59185327a6f3adcf09a
Ubuntu <disha.espsofttech@gmail.com>
2023-01-23 17:25:02 +0530

    transaction completed

--- models/transactionModel.js
+++ models/transactionModel.js
@@ -13,9 +13,21 @@ const promisePool = pool.promise();
 
 class transactionModel {
   insertDetails = async (data) => {
-    let sql = `INSERT INTO transaction(user_id,coin_id,buyCoin_Id,type,amount)VALUES('${data.user_id}','${data.coin_id}','${data.buyCoin_Id}','${data.type}','${data.amount}')`;
+    let sql = `INSERT INTO transaction(user_id,coin_id,buyCoin_Id,type,amount,buyAmount)VALUES('${data.user_id}','${data.coin_id}','${data.buyCoin_Id}','${data.type}','${data.amount}','${data.buyAmount}')`;
     const [result, fields] = await promisePool.query(sql);
+    
     return result;
   };
+
+  getAllTransactionDetail=async()=>{
+    let sql = `SELECT  transaction.user_id,transaction.coin_id,transaction.buyCoin_Id,transaction.type,transaction.amount,transaction.buyAmount,users.first_name ,coin.coinName , coins.coinName as buyCoinName from transaction
+     LEFT JOIN users ON transaction.user_id=users.id
+      LEFT JOIN coins  as coin ON  transaction.coin_id=coin.id 
+      LEFT JOIN coins  ON transaction.buyCoin_Id = coins.id`;
+    const [result, fields] = await promisePool.query(sql);
+    return result;
+  }
+
+
 }
 module.exports = new transactionModel();
